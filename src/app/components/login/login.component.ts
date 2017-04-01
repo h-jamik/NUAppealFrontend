a696a5a77efa3b01e1cba7a0400866f70ad4b475
Jamshed Haitov <h.jamik@gmail.com>
2017-04-02 00:04:15 +0600

    Socket issue resolved

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -23,55 +23,56 @@ export class LoginComponent implements OnInit {
   constructor(
     private router: Router,
     private service: AuthenticationService,
-    private sailsService: SailsService
+    private socket: SailsService
   ) {}
 
   ngOnInit() {
-
-
-    //console.log("sails ", this.sailsService);
-    //this.sailsService.connect();
-    //console.log("sails ", this.sailsService);
-
-    this.sailsService.connect('http://localhost:1337').subscribe(res => {
-      console.log("connected! ", res);
-    }, err => {
-      console.log("error! ", err);
-    });
-
-    this.sailsService.on('hello').subscribe( data => {
-      console.log('Socket `' + data.id + '` joined the party!');
-    });
-
-    /*io.socket.on('hello', function (data) {
-      console.log('Socket `' + data.id + '` joined the party!');
-    });*/
-
     localStorage.removeItem("token");
     localStorage.removeItem("user");
   }
 
   signin() {
 
-
-
-    this.sailsService.get('/appeal/myfunctest').subscribe(resp => {
-      console.log("resp ", resp);
-    });
-
-    /*if (this.email != '' && this.password != '') {
+    if (this.email != '' && this.password != '') {
       this.service.login(this.email, this.password).subscribe(res => {
         if (res.user && res.token) {
           localStorage.setItem("token", res.token);
           localStorage.setItem("user", JSON.stringify(res.user));
+
+          this.connectSocket();
+
           this.router.navigate(['/main']);
+
         } else {
           this.error = false;
         }
       }, err => {
         this.error = true;
       });
-    }*/
+    }
+  }
+
+  connectSocket() {
+    let options = {
+      url: 'http://localhost:1337',
+      headers: {Authorization: "Bearer "+localStorage.getItem("token")}
+    };
+
+    this.socket.connect(options).subscribe(res => {
+      console.log("connected! ", res);
+
+      this.socket.get('/authentication/subscribeUser').subscribe(resp => {
+        console.log("resp ", resp);
+      });
+
+    }, err => {
+      console.log("error! ", err);
+    });
+
+
+    // this.socket.on("hello").subscribe(res => {
+    //   console.log("hello! ", res);
+    // });
   }
 
 }
